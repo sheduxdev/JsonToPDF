92b5d867fe49d3e6c9ce2fae71bf8416caded4f0
Deniz USTA <ddenizustaa@outlook.com.tr>
2023-12-04 12:33:39 +0300

    Update dataChecker.js

--- dataChecker.js
+++ dataChecker.js
@@ -5,30 +5,37 @@ const fetchData = async () => {
     const response = await axios.get('http://oriongida.com/wp-json/custom/v2/products');
     return response.data;
   } catch (error) {
-    console.error('Veri çekme hatası:', error);
-    return null;
+    console.error('Veri çekme hatası:', error.message);
+    return { error: 'Veri çekme hatası', details: error.message };
   }
 };
 
 const getData = async () => {
-  const remoteData = await fetchData();
+  try {
+    const remoteData = await fetchData();
 
-  if (remoteData) {
-    return {
-      items: remoteData,
-    };
-  } else {
-    return {
-      items: [
-        {
-          "urun_adi": "Lütfen veri sunucusunu kontrol edin.",
-          "marka": "Lütfen veri sunucusunu kontrol edin.",
-          "cat_name": "Lütfen veri sunucusunu kontrol edin.",
-          "stok_kodu": "504",
-          "img": "https://imgur.com/UQWpx7I.png"
-        }
-      ],
-    };
+    if (remoteData && !remoteData.error) {
+      console.log(`Toplam ${remoteData.length} ürün koyuldu.`);
+
+      return {
+        items: remoteData,
+      };
+    } else {
+      return {
+        items: [
+          {
+            "urun_adi": "Lütfen veri sunucusunu kontrol edin.",
+            "marka": "Lütfen veri sunucusunu kontrol edin.",
+            "cat_name": "Lütfen veri sunucusunu kontrol edin.",
+            "stok_kodu": "504",
+            "img": "https://imgur.com/UQWpx7I.png"
+          }
+        ],
+      };
+    }
+  } catch (error) {
+    console.error('getData fonksiyonunda bir hata oluştu:', error.message);
+    return { error: 'getData fonksiyonunda bir hata oluştu', details: error.message };
   }
 };
 
